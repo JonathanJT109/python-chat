4077b0227491a6d3c5083c41d08f81e894143991
Jonathan Gonzalez <jonathangon.2014@gmail.com>
2023-10-02 23:39:24 -0400

    Version for Assignment(2) Add requirements. Change colors.

--- server.py
+++ server.py
@@ -14,9 +14,7 @@ clients = {}
 init()
 colors = [Fore.BLUE, Fore.CYAN, Fore.GREEN, Fore.LIGHTBLACK_EX,
           Fore.LIGHTBLUE_EX, Fore.LIGHTCYAN_EX, Fore.LIGHTGREEN_EX,
-          Fore.LIGHTMAGENTA_EX, Fore.LIGHTRED_EX, Fore.LIGHTWHITE_EX,
-          Fore.LIGHTYELLOW_EX, Fore.MAGENTA, Fore.WHITE, Fore.YELLOW
-          ]
+          Fore.LIGHTMAGENTA_EX, Fore.LIGHTYELLOW_EX, Fore.MAGENTA, Fore.YELLOW]
 
 
 class Client:
