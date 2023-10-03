241a38a30df8c9e56898074dc5fc6b74bc2160f3
Jonathan Gonzalez <jonathangon.2014@gmail.com>
2023-10-02 23:34:40 -0400

    Version for Assignment

---
+++ client.py
@@ -0,0 +1,67 @@
+import socket
+import threading
+import time
+
+SERVER_PORT = 5555
+SERVER_HOST = "127.0.0.1"
+
+sock_ready = False
+status = True
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+
+def listen_thread():
+    global s
+    global sock_ready
+    global status
+    while not sock_ready:
+        time.sleep(0.1)
+    while status:
+        (msg, addr) = s.recvfrom(1024)
+        msg = msg.decode('UTF-8')
+
+        if msg == 'ping!':
+            s.sendto("active".encode('UTF-8'), (SERVER_HOST, SERVER_PORT))
+        else:
+            print(msg)
+
+
+welcome_message = """
+Welcome to the Chat Room!
+Please enter your name and a message to enter.
+If you want to send a private message use "/msg" followed by the name and the message.
+
+                    "/msg John Hello, how are you?"
+                    
+If you want to ban someone from texting in the chat room use "/ban" followed by the name.
+You will need at least one more person to agree with your action. The user can still view
+public conversations but is not able to type.
+
+If you want to exit use the "/exit" command.
+"""
+
+print(welcome_message)
+
+while True:
+    name = input("Enter you name: ")
+    if len(name) > 0:
+        break
+
+s.sendto(f'user_name: {name}'.encode('UTF-8'), (SERVER_HOST, SERVER_PORT))
+
+thread = threading.Thread(target=listen_thread).start()
+
+print("-" * 20)
+print("Start Typing")
+print("-" * 20)
+while True:
+    user_input = input()
+    if user_input == "/exit":
+        print("You left the chat!")
+        status = False
+        break
+
+    message = user_input
+
+    s.sendto(message.encode('UTF-8'), (SERVER_HOST, SERVER_PORT))
+    sock_ready = True
--- server.py
+++ server.py
@@ -1,48 +1,99 @@
 import socket
-from threading import Thread
+import threading
+from datetime import datetime
+import random
+from colorama import Fore, init, Back
 import time
 
-SERVER_PORT = 5555
-separator_token = "<SEP>"
-
-client_sockets = set()
+listen_port = 5555  # anything over 1024 
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind(('0.0.0.0', SERVER_PORT))
+s.bind(('0.0.0.0', listen_port))
+clients = {}
 
-def listen_for_client(cs):
-    """
-    This function keep listening for a message from `cs` socket
-    Whenever a message is received, broadcast it to all other connected clients
-    """
+init()
+colors = [Fore.BLUE, Fore.CYAN, Fore.GREEN, Fore.LIGHTBLACK_EX,
+          Fore.LIGHTBLUE_EX, Fore.LIGHTCYAN_EX, Fore.LIGHTGREEN_EX,
+          Fore.LIGHTMAGENTA_EX, Fore.LIGHTRED_EX, Fore.LIGHTWHITE_EX,
+          Fore.LIGHTYELLOW_EX, Fore.MAGENTA, Fore.WHITE, Fore.YELLOW
+          ]
+
+
+class Client:
+    def __init__(self, name, active_time, ban_counter, status, color):
+        self.name = name
+        self.active_time = active_time
+        self.color = color
+        self.status = status
+        self.ban_counter = ban_counter
+
+
+def listen_for_client():
+    global s
+    msg = "ping!"
     while True:
-        msg = ""
-        try:
-            # keep listening for a message from `cs` socket
-            msg = cs.recv(1024).decode()
-        except Exception as e:
-            # client no longer connected
-            # remove it from the set
-            print(f"[!] Error: {e}")
-            client_sockets.remove(cs)
-        else:
-            # if we received a message, replace the <SEP> 
-            # token with ": " for nice printing
-            msg = msg.replace(separator_token, ": ")
-        # iterate over all connected sockets
-        for client_socket in client_sockets:
-            # and send the message
-            client_socket.send(msg.encode())
+        del_list = []
+        time.sleep(5)
+
+        for client in clients.keys():
+            s.sendto(msg.encode('UTF-8'), client)
+
+        for addr in clients.keys():
+            if clients[addr].active_time + 10 < time.time():
+                del_list.append(addr)
+                print(f"{addr}: {clients[addr].name} got removed!")
+
+        for addr in del_list:
+            del clients[addr]
+
+
+thread = threading.Thread(target=listen_for_client).start()
 
 while True:
-    # we keep listening for new connections all the time
-    client_socket, client_address = s.accept()
-    print(f"[+] {client_address} connected.")
-    # add the new connected client to connected sockets
-    client_sockets.add(client_socket)
-    # start a new thread that listens for each client's messages
-    t = Thread(target=listen_for_client, args=(client_socket,))
-    # make the thread daemon so it ends whenever the main thread ends
-    t.daemon = True
-    # start the thread
-    t.start()
+    msg, addr = s.recvfrom(1024)
+    msg = msg.decode('UTF-8')
+    date_now = datetime.now().strftime('%m-%d-%Y %H:%M:%S')
+
+    if "user_name:" in msg:
+        msg = msg.replace("user_name: ", "")
+        client_color = random.choice(colors)
+        new_client = Client(msg, time.time(), set(), "online", client_color)
+        clients[addr] = new_client
+        continue
+
+    if "/msg" in msg:
+        private_message = msg.split(" ")
+        user_found = False
+        for address in clients.keys():
+            if clients[address].name == private_message[1]:
+                msg = msg.replace(f'{private_message[0]} {private_message[1]} ', "")
+                send_message = f'(PRIVATE) {clients[addr].color}[{date_now}] {clients[addr].name}: {msg}{Fore.RESET}'
+                s.sendto(send_message.encode('UTF-8'), address)
+                user_found = True
+                break
+        if not user_found:
+            s.sendto("User has not been found".encode('UTF-8'), addr)
+        continue
+    elif "/ban" in msg:
+        private_message = msg.split(" ")
+        for address in clients.keys():
+            if clients[address].name == private_message[1]:
+                clients[address].ban_counter.add(addr)
+                if len(clients[address].ban_counter) > 1:
+                    s.sendto(f"{Fore.RED}--YOU HAVE BEEN BANNED FROM THE CHAT--{Fore.RESET}".encode('UTF-8'), address)
+                    clients[address].status = "banned"
+                break
+        continue
+
+    clients[addr].active_time = time.time()
+
+    if msg == "active" or clients[addr].status == "banned":
+        continue
+
+    send_message = f'{clients[addr].color}[{date_now}] {clients[addr].name}: {msg}{Fore.RESET}'
+    print(send_message)
+
+    for c in clients.keys():
+        if c == addr:
+            continue
+        s.sendto(send_message.encode('UTF-8'), c)
